c180b7c4f2c9fa46aba012bd176cc68b7e15d421
Salief Lewis <salieflewis@gmail.com>
2022-10-31 15:46:51 -0400

    first pass

---
+++ zora-tiered-pricing-minter/src/CustomPricingMinterFactory.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {CustomPricingMinter} from "./CustomPricingMinter.sol";
+import {Clones} from "openzeppelin-contracts/proxy/Clones.sol";
+
+contract CustomPricingMinterFactory {
+    // The address of the base CustomPricingMinter implementation
+    address public immutable customPricingMinterImpl;
+
+    event CustomPricingMinterCreated(address newCustomPricingMinter, address deployer);
+
+    /**
+     * @notice Default constructor.
+     */
+    constructor(address _customPricingMinterImpl) {
+        customPricingMinterImpl = _customPricingMinterImpl;
+    }
+
+    /**
+     * @notice Initializes a new CustomPricingMinter contract using the CREATE opcode.
+     */
+    function createCustomPricingMinter(
+        uint256 _nonBundlePricePerToken,
+        uint256 _bundlePricePerToken,
+        uint256 _bundleQuantity
+    ) external returns (address newCustomPricingMinter) {
+        newCustomPricingMinter = Clones.clone(customPricingMinterImpl);
+        emit CustomPricingMinterCreated(newCustomPricingMinter, msg.sender);
+    }
+}
